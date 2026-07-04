28545fc5af6b2ecd8e72854d6ad7bf571668b909
ESHANI BANIK <banikesha03@gmail.com>
2026-07-04 23:58:14 +0530

    Update 6. Move Zeroes to end(LC-283).py

--- Array/EASY/6. Move Zeroes to end(LC-283).py
+++ Array/EASY/6. Move Zeroes to end(LC-283).py
@@ -1,1 +1,9 @@
-
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        n=len(nums)
+        l = 0
+        for r in range(len(nums)):
+            if nums[r] != 0:
+                # Swap the elements
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
