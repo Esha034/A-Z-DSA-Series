f0b6d6a4d1de14e3888765e629572912c9a79258
ESHANI BANIK <banikesha03@gmail.com>
2026-08-16 12:37:05 +0530

    Update 5. Squares of a Sorted Array(LC-977).py

--- 1. Two Pointer Approach/5. Squares of a Sorted Array(LC-977).py
+++ 1. Two Pointer Approach/5. Squares of a Sorted Array(LC-977).py
@@ -91,17 +91,23 @@ Since each element is processed exactly once, the time complexity is O(n)
 
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
+		
         n=len(nums)
         ans=[0]*n
         left,right=0,n-1
+		
         for i in range(n-1,-1,-1):
+			
             if abs(nums[left])<abs(nums[right]):
                 ans[i]=nums[right]*nums[right]
                 right-=1
+				
             else:
                 ans[i]=nums[left]*nums[left]
                 left+=1
         return ans
+
+
 #complexity
 
 Time Complexity=O(n)
