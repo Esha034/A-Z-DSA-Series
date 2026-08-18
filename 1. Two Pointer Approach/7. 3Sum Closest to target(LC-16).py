7d627aa3a05c4605242729bb758f0452b7e6584b
ESHANI BANIK <banikesha03@gmail.com>
2026-08-18 09:08:37 +0530

    Update 7. 3Sum Closest to target(LC-16).py

--- 1. Two Pointer Approach/7. 3Sum Closest to target(LC-16).py
+++ 1. Two Pointer Approach/7. 3Sum Closest to target(LC-16).py
@@ -1,3 +1,40 @@
+Approach 1 — Brute Force
+
+Try every possible combination of 3 elements.
+
+
+#code
+
+    class Solution:
+    def threeSumClosest(self, nums, target):
+        n = len(nums)
+        closest = nums[0] + nums[1] + nums[2]
+
+        for i in range(n):
+            for j in range(i + 1, n):
+                for k in range(j + 1, n):
+
+                    total = nums[i] + nums[j] + nums[k]
+
+                    if abs(total - target) < abs(closest - target):
+                        closest = total
+
+        return closest
+
+
+#complexity
+
+Time  : O(n³)
+Space : O(1)
+
+
+
+Approach 3 — Optimal:  Sorting + Two Pointers
+
+
+
+#code
+
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
         nums.sort()
@@ -26,4 +63,9 @@ class Solution:
                     k-=1
 
         return res_sum
-        
+
+
+#complexity
+
+Time  : O(n²)
+Space : O(1)
