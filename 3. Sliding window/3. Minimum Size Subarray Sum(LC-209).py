95c3fd5fc99f012d54421e0d55fa41598775aeea
ESHANI BANIK <banikesha03@gmail.com>
2026-08-20 15:42:27 +0530

    Update 3. Minimum Size Subarray Sum(LC-209).py

--- 3. Sliding window/3. Minimum Size Subarray Sum(LC-209).py
+++ 3. Sliding window/3. Minimum Size Subarray Sum(LC-209).py
@@ -1,1 +1,17 @@
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+
+        n=len(nums)
+        low,high=0,0
+        curr_sum=0
+        res=float("inf")
+    
+        while high<n:
+            curr_sum+=nums[high]
+            while curr_sum>=target:
+                res=min(res,(high-low+1))
+                curr_sum-=nums[low]
+                low+=1
+            high+=1
+        return res if res != float("inf") else 0
 
