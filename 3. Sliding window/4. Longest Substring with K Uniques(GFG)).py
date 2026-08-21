a1830582dc59e69d865517b8bf6f7ae7b2d7557d
ESHANI BANIK <banikesha03@gmail.com>
2026-08-21 09:54:06 +0530

    Update 4. Longest Substring with K Uniques(GFG)).py

--- 3. Sliding window/4. Longest Substring with K Uniques(GFG)).py
+++ 3. Sliding window/4. Longest Substring with K Uniques(GFG)).py
@@ -1,3 +1,5 @@
+#My code
+
 class Solution:
     def longestKSubstr(self, s, k):
         
@@ -24,3 +26,31 @@ class Solution:
                 low+=1
                 
         return res 
+
+
+#optimal code
+
+class Solution:
+    def longestKSubstr(self, s, k):
+        
+        n=len(s)
+        low=0
+        freq={}
+        res=-1
+        
+        for high in range(n):
+                 
+            freq[s[high]]=freq.get(s[high],0)+1
+            
+            if len(freq)==k:
+                res=max(res,high-low+1)
+                
+            while len(freq)>k:
+                freq[s[low]]-=1
+                if freq[s[low]]==0:
+                    del freq[s[low]]
+                low+=1
+                
+                
+        return res 
+    
