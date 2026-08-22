a0e8c1a10bbff0f5fa66c2607c08eccd20b5fad5
ESHANI BANIK <banikesha03@gmail.com>
2026-08-22 11:38:38 +0530

    Update 5. Fruit Into Baskets(LC-904).py

--- 3. Sliding window/5. Fruit Into Baskets(LC-904).py
+++ 3. Sliding window/5. Fruit Into Baskets(LC-904).py
@@ -1,1 +1,26 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        n=len(fruits)
+        basket={}
+        low,high=0,0
+        res=0
 
+        for high in range(n):
+            if fruits[high] in basket:
+                basket[fruits[high]]+=1
+            else:
+                basket[fruits[high]]=1
+
+            if len(basket)<=2:
+                res=max(res,high-low+1)
+
+            while len(basket)>2:
+                basket[fruits[low]]-=1
+                if basket[fruits[low]]==0:
+                    del basket[fruits[low]]
+                low+=1
+        return res
+
+
+
+        
