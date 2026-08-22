fe1067ec67545708664c1238186c705a509253cd
ESHANI BANIK <banikesha03@gmail.com>
2026-08-22 12:22:10 +0530

    Update 6. Longest Substring Without Repeating Characters(LC-3).py

--- 3. Sliding window/6. Longest Substring Without Repeating Characters(LC-3).py
+++ 3. Sliding window/6. Longest Substring Without Repeating Characters(LC-3).py
@@ -1,3 +1,4 @@
+#my code
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
        
@@ -17,6 +18,30 @@ class Solution:
                     del freq[s[low]]
                 low+=1
         return maxlen 
+
+
+#optimal code
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        last_seen = {}
+        low = 0
+        max_len = 0
+
+        for high in range(len(s)):
+
+            if s[high] in last_seen:
+                if last_seen[s[high]] >= low:
+                    low = last_seen[s[high]] + 1
+
+            last_seen[s[high]] = high
+
+            length = high - low + 1
+
+            if length > max_len:
+                max_len = length
+
+        return max_len
 
 
 
