c77a1131178eee474ae5ab51e973c63691f29553
ESHANI BANIK <banikesha03@gmail.com>
2026-09-01 17:39:22 +0530

    Update 4. 0 - 1 Knapsack Problem(GFG).py

--- Dynamic Programming Series/4. 0 - 1 Knapsack Problem(GFG).py
+++ Dynamic Programming Series/4. 0 - 1 Knapsack Problem(GFG).py
@@ -2,3 +2,36 @@ Compute dfs(1,0)
 Capacity
 
 0
+
+1. Bruteforce: Memoization (Top-Down DP approach)
+
+class Solution:
+    def knapsack(self, W: int, val: list[int], wt: list[int]) -> int:
+        n=len(val)
+        dp={}
+        
+        def dfs(i,W):
+            
+            if i==n:
+                return 0
+                
+            if (i,W) in dp:
+                return dp[(i,W)]
+                
+            skip=dfs(i+1,W)
+            
+            take=0
+            
+            if wt[i]<=W:
+                take=val[i]+dfs(i+1,W-wt[i])
+                
+            dp[(i,W)]=max(take,skip)
+            
+            return dp[(i,W)]
+            
+        return dfs(0,W)
+
+
+2. Better: Tabulation ( Bottom-Up DP approach)
+
+3. Optimal: Space Optimized DP approach
