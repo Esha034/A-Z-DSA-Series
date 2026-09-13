01a1d934fd9624c719a5971b9e231d5cce1c571b
ESHANI BANIK <banikesha03@gmail.com>
2026-09-13 18:18:54 +0530

    Update 10. Minimum Window Substring(LC-76).py

--- 3. Sliding window/10. Minimum Window Substring(LC-76).py
+++ 3. Sliding window/10. Minimum Window Substring(LC-76).py
@@ -1,54 +1,55 @@
+#optimal solution-using freq map
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        m = len(s)
-        n = len(t)
-
-        if m < n:
+        if len(s) < len(t):
             return ""
 
-        # Frequency of characters required from t
-        freq = [0] * 128
+        freq = {}
 
+        # Store required frequency of characters in t
         for ch in t:
-            freq[ord(ch)] += 1
+            if ch in freq:
+                freq[ch] += 1
+            else:
+                freq[ch] = 1
 
         low = 0
-        required = n
+        required = len(t)
 
-        min_len = m + 1
+        min_len = float("inf")
         start = 0
 
-        for high in range(m):
+        for high in range(len(s)):
 
-            # Add s[high] to the window
-            index = ord(s[high])
-
-            # This character was still required
-            if freq[index] > 0:
+            # If s[high] is still required
+            if s[high] in freq and freq[s[high]] > 0:
                 required -= 1
 
-            freq[index] -= 1
+            # Decrease frequency
+            if s[high] in freq:
+                freq[s[high]] -= 1
 
-            # Window contains all characters of t
+            # Window is valid
             while required == 0:
 
                 curr_len = high - low + 1
 
+                # Update minimum window
                 if curr_len < min_len:
                     min_len = curr_len
                     start = low
 
-                # Remove s[low]
-                index = ord(s[low])
-                freq[index] += 1
+                # Remove left character
+                if s[low] in freq:
+                    freq[s[low]] += 1
 
-                # We have now lost a required character
-                if freq[index] > 0:
-                    required += 1
+                    # We removed a required character
+                    if freq[s[low]] > 0:
+                        required += 1
 
                 low += 1
 
-        if min_len == m + 1:
+        if min_len == float("inf"):
             return ""
 
         return s[start:start + min_len]
@@ -59,5 +60,72 @@ class Solution:
 
 
 
+
+
+#Optimal Solution
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        m=len(s)
+        n=len(t)
+
+        if m<n:
+            return ""
+            
+        # Frequency of characters required from t
+        freq=[0]*128
+
+        for ch in t:
+            freq[ord(ch)]+=1
+
+        low=0
+        start=0
+        required=n
+        minlen=float("inf")
+
+        for high in range(m):
+
+            index=ord(s[high])
+
+            if freq[index]>0:
+                required-=1
+
+            freq[index]-=1
+
+            while required==0:
+
+                currlen=high-low+1
+
+                if currlen < minlen:
+                    minlen = currlen
+                    start = low
+                # Remove s[low]
+                index=ord(s[low])
+                freq[index]+=1
+
+                if freq[index]>0:
+                    required+=1
+
+                low+=1
+
+        if minlen==float("inf"):
+            return ""
+
+        return s[start:start+minlen]
+
+
+Complexity:
+
+Time: O(m + n)
+Building frequency: O(n)
+Scanning s:O(m)
+Where, Each character enters and leaves the window at most once.
+
+
+Space: O(1)
+The frequency array has fixed size 128:
+
         
+
+
+
         
