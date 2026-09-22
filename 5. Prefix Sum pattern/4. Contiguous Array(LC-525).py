ee3591a7b2e1e3d2b49060ab4b93f9fc690dfa9b
ESHANI BANIK <banikesha03@gmail.com>
2026-09-22 11:36:33 +0530

    Update and rename 4. Prefix sum problem.py to 4. Contiguous Array(LC-525).py

---
+++ 5. Prefix Sum pattern/4. Contiguous Array(LC-525).py
@@ -0,0 +1,23 @@
+class Solution:
+    def findMaxLength(self, nums: List[int]) -> int:
+        n=len(nums)
+        zeroes=0
+        ones=0
+        res=0
+        diffmap={}
+        for i in range(n):
+            if nums[i]==0:
+                zeroes+=1
+            if nums[i]==1:
+                ones+=1
+
+            diff=zeroes-ones
+
+            if diff==0:
+                res=max(res,i+1)
+            if diff in diffmap:
+                length=i-diffmap[diff]
+                res=max(res,length)
+            else:
+                diffmap[diff]=i
+        return res
